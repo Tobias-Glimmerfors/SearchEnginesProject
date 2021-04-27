926c71b8fccb5faba78196109b7ca2e7af853feb
Rikard Franksson <rikard.franksson@gmail.com>
2021-04-27 13:56:56 +0200

    change structure

--- src/Engine.java
+++ src/Engine.java
@@ -1,11 +1,10 @@
-/**
- * Hello world!
- *
- */
+
+
 public class Engine 
 {
     public static void main( String[] args )
     {
+        Searcher searcher = new Searcher();
         System.out.println( "Hello World!" );
     }
 }
